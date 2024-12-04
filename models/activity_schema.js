8dd702a7bec7b8cd18ce2112238565611a32961b
Jérémy DASI <jeremy.dasi@gmail.com>
2024-12-04 17:24:24 +0100

    router

--- models/activity_schema.js
+++ models/activity_schema.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const activitySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    localisation: { type: String, required: true },
-    type: { type: String, required: true },
-    price: { type: Number, required: true },
-})
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  localisation: { type: String, required: true },
+  type: { type: String, required: true },
+  price: { type: Number, required: true },
+});
 
-module.exports = mongoose.model('Activity', activitySchema);
+module.exports = mongoose.model("Activity", activitySchema);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const routeActivity = require("./routes/routeActivity");
 
 dotenv.config();
 
@@ -13,17 +14,16 @@ app.use(cors());
 app.use(bodyParser.json());
 
 mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URL)
   .then(() => console.log("Connecté à MongoDB"))
   .catch((err) => console.error("Erreur de connexion à MongoDB", err));
 
-app.get('/', (req, res) => {
-    res.send("Bienvenue sur le site de recherche d'activité");
-})
+app.use('/activities', routeActivity);
+
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur le site de recherche d'activité");
+});
 
 app.listen(PORT, () => {
-    console.log(`Serveur connecté sur le port ${PORT}`)
-})
+  console.log(`Serveur connecté sur le port http://localhost:${PORT}`);
+});
